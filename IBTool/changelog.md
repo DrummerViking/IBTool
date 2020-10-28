b99ef074aa2d608327b9a2971ec532cb94d17a6e
Agustin Gallegos <agallego@microsoft.com>
2020-10-28 19:47:55 -0300

    update to version 1.0.13

--- IBTool/changelog.md
+++ IBTool/changelog.md
@@ -1,10 +1,11 @@
 ﻿# Changelog  
 
-## 1.0.11 (2020-10-28)  
+## 1.0.13 (2020-10-28)  
  - New: Added 5 functions  
  - New: Added display information for Audit logs and Exchange ABP.  
  - New: Added 3 buttons to call the functions.  
- - Fixed: Remove help comments from install.ps1 function.
+ - Update: Update Connect-OnlineServices function to use a Switch method and connect to passed Services by parameter.  
+ - Fixed: Remove help comments from install.ps1 function.  
 
 ## 1.0.0 (2020-10-27)  
  - New: Project Start
--- IBTool/internal/functions/Connect-OnlineServices.ps1
+++ IBTool/internal/functions/Connect-OnlineServices.ps1
@@ -9,26 +9,8 @@
     .PARAMETER Credential
     Credential to use for the connection.
 
-    .PARAMETER EXO
-    Use this switch parameter to connect to Exchange Online remote powershell.
-
-    .PARAMETER SCC
-    Use this switch parameter to connect to Security and Compliance remote powershell.
-
-    .PARAMETER MicrosoftTeams
-    Use this switch parameter to connect to MS Teams module.
-
-    .PARAMETER MSOnline
-    Use this switch parameter to connect to MS Online module.
-
-    .PARAMETER AzureAD
-    Use this switch parameter to connect to Azure AD module.
-
-    .PARAMETER AzureADPreview
-    Use this switch parameter to connect to Azure AD Preview module.
-
-    .PARAMETER Azure
-    Use this switch parameter to connect to Azure module.
+    .PARAMETER Services
+    List of the desired services to connect to. Current available services: EXO, SCC, MicrosoftTeams, MSOnline, AzureAD, AzureADPreview, Azure.
 
     .PARAMETER Confirm
     If this switch is enabled, you will be prompted for confirmation before executing any operations that change state.
@@ -47,26 +29,9 @@
         [PSCredential]
         $Credential = (Get-Credential -Message "Please specify O365 Global Admin Credentials"),
 
-        [Switch]
-        $EXO,
-
-        [Switch]
-        $SCC,
-
-        [Switch]
-        $MicrosoftTeams,
-
-        [Switch]
-        $MSOnline,
-
-        [Switch]
-        $AzureAD,
-
-        [Switch]
-        $AzureADPreview,
-
-        [Switch]
-        $Azure
+        [ValidateSet('EXO', 'SCC', 'MicrosoftTeams', 'MSOnline', 'AzureAD', 'AzureADPreview', 'Azure')]
+        [String[]]
+        $Services
     )
     if(-not $Credential){
         $Credential = Get-Credential -Message "Please specify O365 Global Admin Credentials"
@@ -75,142 +40,144 @@
         Stop-PSFFunction -Message "Credentials entered are invalid." -EnableException $true -Cmdlet $PSCmdlet
     }
 
-    if($Azure){
-        Invoke-PSFProtectedCommand -Action "Connecting to Azure" -Target "Azure" -ScriptBlock {
-            Write-PSFHostColor -String  "[$((Get-Date).ToString("HH:mm:ss"))] Connecting to Azure"
-            Install-Module Azure -Force -ErrorAction Stop
-            Import-Module Azure -ErrorAction Stop
-        } -EnableException $true -PSCmdlet $PSCmdlet
-    }
+    Switch ( $Services ) {
+        Azure {
+            Invoke-PSFProtectedCommand -Action "Connecting to Azure" -Target "Azure" -ScriptBlock {
+                Write-PSFHostColor -String  "[$((Get-Date).ToString("HH:mm:ss"))] Connecting to Azure"
+                Install-Module Azure -Force -ErrorAction Stop
+                Import-Module Azure -ErrorAction Stop
+            } -EnableException $true -PSCmdlet $PSCmdlet
+        }
 
-    if($AzureAD){
-        Invoke-PSFProtectedCommand -Action "Connecting to AzureAD" -Target "AzureAD" -ScriptBlock {
-            Write-PSFHostColor -String  "[$((Get-Date).ToString("HH:mm:ss"))] Connecting to AzureAD"
-            if ( !(Get-Module AzureAD -ListAvailable) -and !(Get-Module AzureAD) ) {
-                Install-Module AzureAD -Force -ErrorAction Stop
-            }
-            try {
-                Import-module AzureAD
-                $null = Connect-AzureAD -Credential $Credential -ErrorAction Stop
-            }
-            catch {
-                if ( ($_.Exception.InnerException.InnerException.InnerException.InnerException.ErrorCode | ConvertFrom-Json).error -eq 'interaction_required' ) {
-                    Write-PSFHostColor -String  "[$((Get-Date).ToString("HH:mm:ss"))] Your are account seems to be requiring MFA to connect to Azure AD. Requesting to authenticate"
-                    $null = Connect-AzureAD -AccountId $Credential.UserName.toString() -ErrorAction Stop
+        AzureAD {
+            Invoke-PSFProtectedCommand -Action "Connecting to AzureAD" -Target "AzureAD" -ScriptBlock {
+                Write-PSFHostColor -String  "[$((Get-Date).ToString("HH:mm:ss"))] Connecting to AzureAD"
+                if ( !(Get-Module AzureAD -ListAvailable) -and !(Get-Module AzureAD) ) {
+                    Install-Module AzureAD -Force -ErrorAction Stop
                 }
-                else {
-                    return $_
+                try {
+                    Import-module AzureAD
+                    $null = Connect-AzureAD -Credential $Credential -ErrorAction Stop
                 }
-            }
-        } -EnableException $true -PSCmdlet $PSCmdlet
-    }
-
-    if($AzureADPreview){
-        Invoke-PSFProtectedCommand -Action "Connecting to AzureAD Preview" -Target "AzureAD" -ScriptBlock {
-            Write-PSFHostColor -String  "[$((Get-Date).ToString("HH:mm:ss"))] Connecting to AzureAD Preview"
-            if ( !(Get-Module AzureADPreview -ListAvailable) -and !(Get-Module AzureADPreview) ) {
-                Install-Module AzureADPreview -Force -ErrorAction Stop
-            }
-            try {
-                Import-module AzureADPreview
-                $null = Connect-AzureAD -Credential $Credential -ErrorAction Stop
-            }
-            catch {
-                if ( ($_.Exception.InnerException.InnerException.InnerException.InnerException.ErrorCode | ConvertFrom-Json).error -eq 'interaction_required' ) {
-                    Write-PSFHostColor -String  "[$((Get-Date).ToString("HH:mm:ss"))] Your are account seems to be requiring MFA to connect to Azure AD. Requesting to authenticate"
-                    $null = Connect-AzureAD -AccountId $Credential.UserName.toString() -ErrorAction Stop
+                catch {
+                    if ( ($_.Exception.InnerException.InnerException.InnerException.InnerException.ErrorCode | ConvertFrom-Json).error -eq 'interaction_required' ) {
+                        Write-PSFHostColor -String  "[$((Get-Date).ToString("HH:mm:ss"))] Your are account seems to be requiring MFA to connect to Azure AD. Requesting to authenticate"
+                        $null = Connect-AzureAD -AccountId $Credential.UserName.toString() -ErrorAction Stop
+                    }
+                    else {
+                        return $_
+                    }
                 }
-                else {
-                    return $_
+            } -EnableException $true -PSCmdlet $PSCmdlet
+        }
+
+        AzureADPreview {
+            Invoke-PSFProtectedCommand -Action "Connecting to AzureAD Preview" -Target "AzureAD" -ScriptBlock {
+                Write-PSFHostColor -String  "[$((Get-Date).ToString("HH:mm:ss"))] Connecting to AzureAD Preview"
+                if ( !(Get-Module AzureADPreview -ListAvailable) -and !(Get-Module AzureADPreview) ) {
+                    Install-Module AzureADPreview -Force -ErrorAction Stop
                 }
-            }
-        } -EnableException $true -PSCmdlet $PSCmdlet
-    }
+                try {
+                    Import-module AzureADPreview
+                    $null = Connect-AzureAD -Credential $Credential -ErrorAction Stop
+                }
+                catch {
+                    if ( ($_.Exception.InnerException.InnerException.InnerException.InnerException.ErrorCode | ConvertFrom-Json).error -eq 'interaction_required' ) {
+                        Write-PSFHostColor -String  "[$((Get-Date).ToString("HH:mm:ss"))] Your are account seems to be requiring MFA to connect to Azure AD. Requesting to authenticate"
+                        $null = Connect-AzureAD -AccountId $Credential.UserName.toString() -ErrorAction Stop
+                    }
+                    else {
+                        return $_
+                    }
+                }
+            } -EnableException $true -PSCmdlet $PSCmdlet
+        }
 
-    if($MSOnline){
-        Invoke-PSFProtectedCommand -Action "Connecting to MSOnline" -Target "MSOnline" -ScriptBlock {
-            Write-PSFHostColor -String  "[$((Get-Date).ToString("HH:mm:ss"))] Connecting to MSOnline"
-            if ( !(Get-Module MSOnline -ListAvailable) -and !(Get-Module MSOnline) ) {
-                Install-Module MSOnline -Force -ErrorAction Stop
-            }
-            try {
-                Import-Module MSOnline
-                Connect-MsolService -Credential $Credential -ErrorAction Stop
-            }
-            catch {
-                Write-PSFHostColor -String  "[$((Get-Date).ToString("HH:mm:ss"))] Your are account seems to be requiring MFA to connect to MS Online. Requesting to authenticate"
-                Connect-MsolService -ErrorAction Stop
-            }
-        } -EnableException $true -PSCmdlet $PSCmdlet
-    }
+        MSOnline {
+            Invoke-PSFProtectedCommand -Action "Connecting to MSOnline" -Target "MSOnline" -ScriptBlock {
+                Write-PSFHostColor -String  "[$((Get-Date).ToString("HH:mm:ss"))] Connecting to MSOnline"
+                if ( !(Get-Module MSOnline -ListAvailable) -and !(Get-Module MSOnline) ) {
+                    Install-Module MSOnline -Force -ErrorAction Stop
+                }
+                try {
+                    Import-Module MSOnline
+                    Connect-MsolService -Credential $Credential -ErrorAction Stop
+                }
+                catch {
+                    Write-PSFHostColor -String  "[$((Get-Date).ToString("HH:mm:ss"))] Your are account seems to be requiring MFA to connect to MS Online. Requesting to authenticate"
+                    Connect-MsolService -ErrorAction Stop
+                }
+            } -EnableException $true -PSCmdlet $PSCmdlet
+        }
 
-    if($MicrosoftTeams){
-        Invoke-PSFProtectedCommand -Action "Connecting to MicrosoftTeams" -Target "MicrosoftTeams" -ScriptBlock {
-            Write-PSFHostColor -String "[$((Get-Date).ToString("HH:mm:ss"))] Connecting to MicrosoftTeams"
-            if ( !(Get-Module MicrosoftTeams -ListAvailable) -and !(Get-Module MicrosoftTeams) ) {
-                Install-Module MicrosoftTeams -Force -ErrorAction Stop
-            }
-            try {
-                #Connect to Microsoft Teams
-                $null = Connect-MicrosoftTeams -Credential $Credential -ErrorAction Stop
-
-                #Connection to Skype for Business Online and import into Ps session
-                $session = New-CsOnlineSession -Credential $Credential -ErrorAction Stop
-                $null = Import-PsSession $session
-            }
-            catch {
-                if ( ($_.Exception.InnerException.InnerException.InnerException.InnerException.ErrorCode | ConvertFrom-Json).error -eq 'interaction_required' ) {
-                    Write-PSFHostColor -String  "[$((Get-Date).ToString("HH:mm:ss"))] Your are account seems to be requiring MFA to connect to MicrosoftTeams. Requesting to authenticate"
+        MicrosoftTeams {
+            Invoke-PSFProtectedCommand -Action "Connecting to MicrosoftTeams" -Target "MicrosoftTeams" -ScriptBlock {
+                Write-PSFHostColor -String "[$((Get-Date).ToString("HH:mm:ss"))] Connecting to MicrosoftTeams"
+                if ( !(Get-Module MicrosoftTeams -ListAvailable) -and !(Get-Module MicrosoftTeams) ) {
+                    Install-Module MicrosoftTeams -Force -ErrorAction Stop
+                }
+                try {
                     #Connect to Microsoft Teams
-                    $null = Connect-MicrosoftTeams -ErrorAction Stop
-
+                    $null = Connect-MicrosoftTeams -Credential $Credential -ErrorAction Stop
+    
                     #Connection to Skype for Business Online and import into Ps session
-                    $session = New-CsOnlineSession -ErrorAction Stop
+                    $session = New-CsOnlineSession -Credential $Credential -ErrorAction Stop
                     $null = Import-PsSession $session
                 }
-                else {
-                    return $_
+                catch {
+                    if ( ($_.Exception.InnerException.InnerException.InnerException.InnerException.ErrorCode | ConvertFrom-Json).error -eq 'interaction_required' ) {
+                        Write-PSFHostColor -String  "[$((Get-Date).ToString("HH:mm:ss"))] Your are account seems to be requiring MFA to connect to MicrosoftTeams. Requesting to authenticate"
+                        #Connect to Microsoft Teams
+                        $null = Connect-MicrosoftTeams -ErrorAction Stop
+    
+                        #Connection to Skype for Business Online and import into Ps session
+                        $session = New-CsOnlineSession -ErrorAction Stop
+                        $null = Import-PsSession $session
+                    }
+                    else {
+                        return $_
+                    }
                 }
-            }
-        } -EnableException $true -PSCmdlet $PSCmdlet
-    }
+            } -EnableException $true -PSCmdlet $PSCmdlet
+        }
 
-    if($EXO){
-        Invoke-PSFProtectedCommand -Action "Connecting to Exchange Online" -Target "EXO" -ScriptBlock {
-            Write-PSFHostColor -String "[$((Get-Date).ToString("HH:mm:ss"))] Connecting to Exchange Online"
-            try {
-                # Getting current PS Sessions
-                $Sessions = Get-PSSession
-                if ($Sessions.ComputerName -eq "outlook.office365.com") { return }
-                else { Connect-ExchangeOnline -Credential $Credential -ErrorAction Stop }
-            }
-            catch {
-                if ( ($_.Exception.InnerException.InnerException.InnerException.InnerException.ErrorCode | ConvertFrom-Json).error -eq 'interaction_required' ) {
-                    Write-PSFHostColor -String  "[$((Get-Date).ToString("HH:mm:ss"))] Your are account seems to be requiring MFA to connect to Exchange Online. Requesting to authenticate"
-                    Connect-ExchangeOnline -UserPrincipalName $Credential.Username.toString() -ErrorAction Stop
+        EXO {
+            Invoke-PSFProtectedCommand -Action "Connecting to Exchange Online" -Target "EXO" -ScriptBlock {
+                Write-PSFHostColor -String "[$((Get-Date).ToString("HH:mm:ss"))] Connecting to Exchange Online"
+                try {
+                    # Getting current PS Sessions
+                    $Sessions = Get-PSSession
+                    if ($Sessions.ComputerName -eq "outlook.office365.com") { return }
+                    else { Connect-ExchangeOnline -Credential $Credential -ErrorAction Stop }
                 }
-                else {
-                    return $_
+                catch {
+                    if ( ($_.Exception.InnerException.InnerException.InnerException.InnerException.ErrorCode | ConvertFrom-Json).error -eq 'interaction_required' ) {
+                        Write-PSFHostColor -String  "[$((Get-Date).ToString("HH:mm:ss"))] Your are account seems to be requiring MFA to connect to Exchange Online. Requesting to authenticate"
+                        Connect-ExchangeOnline -UserPrincipalName $Credential.Username.toString() -ErrorAction Stop
+                    }
+                    else {
+                        return $_
+                    }
                 }
-            }
-        } -EnableException $true -PSCmdlet $PSCmdlet
-    }
+            } -EnableException $true -PSCmdlet $PSCmdlet
+        }
 
-    if($SCC){
-        Invoke-PSFProtectedCommand -Action "Connecting to Security and Compliance" -Target "SCC" -ScriptBlock {
-            Write-PSFHostColor -String "[$((Get-Date).ToString("HH:mm:ss"))] Connecting to Security and Compliance"
-            try {
-                Connect-IPPSSession -Credential $Credential -Prefix SCC -ErrorAction Stop
-            }
-            catch {
-                if ( ($_.Exception.InnerException.InnerException.InnerException.InnerException.ErrorCode | ConvertFrom-Json).error -eq 'interaction_required' ) {
-                    Write-PSFHostColor -String  "[$((Get-Date).ToString("HH:mm:ss"))] Your are account seems to be requiring MFA to connect to Security and Compliance. Requesting to authenticate"
-                    Connect-IPPSSession -UserPrincipalName $Credential.Username.toString() -Prefix SCC -ErrorAction Stop
+        SCC {
+            Invoke-PSFProtectedCommand -Action "Connecting to Security and Compliance" -Target "SCC" -ScriptBlock {
+                Write-PSFHostColor -String "[$((Get-Date).ToString("HH:mm:ss"))] Connecting to Security and Compliance"
+                try {
+                    Connect-IPPSSession -Credential $Credential -ErrorAction Stop
                 }
-                else {
-                    return $_
+                catch {
+                    if ( ($_.Exception.InnerException.InnerException.InnerException.InnerException.ErrorCode | ConvertFrom-Json).error -eq 'interaction_required' ) {
+                        Write-PSFHostColor -String  "[$((Get-Date).ToString("HH:mm:ss"))] Your are account seems to be requiring MFA to connect to Security and Compliance. Requesting to authenticate"
+                        Connect-IPPSSession -UserPrincipalName $Credential.Username.toString() -Prefix SCC -ErrorAction Stop
+                    }
+                    else {
+                        return $_
+                    }
                 }
-            }
-        } -EnableException $true -PSCmdlet $PSCmdlet
+            } -EnableException $true -PSCmdlet $PSCmdlet
+        }
     }
 }
