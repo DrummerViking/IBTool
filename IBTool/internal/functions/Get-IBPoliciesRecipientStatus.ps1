9a88252d7fbff890778f6853fc1594ad9ccc62d7
Agustin Gallegos <agallego@microsoft.com>
2020-10-29 17:05:02 -0300

    update to version 1.0.17

---
+++ IBTool/internal/functions/Get-IBPoliciesRecipientStatus.ps1
@@ -0,0 +1,38 @@
+﻿function Get-IBPoliciesRecipientStatus {
+    <#
+    .SYNOPSIS
+    This funciton gets the current Information Barrier Recipient status.
+    
+    .DESCRIPTION
+    This funciton gets the current Information Barrier Recipient status between 2 users.
+    
+    .PARAMETER User1
+    Defines the first user identity to compare.
+    You can use any value that uniquely identifies each user, such as name, alias, distinguished name, canonical domain name, email address, or GUID.
+
+    .PARAMETER User2
+    Defines the second user identity to compare.
+    You can use any value that uniquely identifies each user, such as name, alias, distinguished name, canonical domain name, email address, or GUID.
+    
+    .EXAMPLE
+    PS C:\> Get-IBPoliciesRecipientStatus -User1 "john@contoso.com" -User2 "mark@contoso.com"
+    This funciton gets the current Information Barrier Recipient status between john@contoso.com and mark@contoso.com.
+    
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, HelpMessage = "Defines the first user identity to compare.")]
+        [string]$user1,
+
+        [Parameter(Mandatory = $true, HelpMessage = "Defines the second user identity to compare.")]
+        [string]$user2
+    )
+    Write-PSFHostColor -String "[$((Get-Date).ToString("HH:mm:ss"))] Getting current information Barrier Recipient status between $user1 and $user2."
+    $statusBar.Text = "Running..."
+    $array = New-Object System.Collections.ArrayList
+    $array.AddRange( (Get-InformationBarrierRecipientStatus -Identity $user1 -Identity2 $user2 -WarningAction SilentlyContinue) )
+    $dataGrid.datasource = $array
+    $dataGrid.AutoResizeColumns()
+    $MainForm.refresh()
+    $statusBar.Text = "Ready. Segments found: $($array.count)"
+}
---
+++ IBTool/internal/functions/Get-SegmentMembers.ps1
@@ -0,0 +1,17 @@
+﻿function Get-SegmentMembers {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseSingularNouns", "")]
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $True, HelpMessage = "Enter Organization Segment Name.")]
+        [String]$SegmentName
+    )
+    Write-PSFHostColor -String "[$((Get-Date).ToString("HH:mm:ss"))] Getting Organization Segment Members."
+    $statusBar.Text = "Running..."
+    $array = New-Object System.Collections.ArrayList
+    $filter = (Get-OrganizationSegment -Identity $SegmentName).UserGroupFilter
+    $array.AddRange( (Get-EXORecipient -Filter $filter -ResultSize Unlimited | Select-Object Name,PrimarySMTPAddress,*recipientType* ) )
+    $dataGrid.datasource = $array
+    $dataGrid.AutoResizeColumns()
+    $MainForm.refresh()
+    $statusBar.Text = "Ready. Members found: $($array.count)"
+}
